0fcbaed3c133b23689d5116050de84315271b2b0
apple <apple@appurunoMacBook-Air.local>
2026-01-04 19:05:36 +0900

    start a new project

---
+++ src/test/kotlin/com/kip/cms/CmsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kip.cms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
