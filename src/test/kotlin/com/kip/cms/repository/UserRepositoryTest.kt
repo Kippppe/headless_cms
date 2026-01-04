9b5564ed11da96da08aa7853d3aaeb2c82267b15
apple <apple@appurunoMacBook-Air.local>
2026-01-04 21:19:08 +0900

    Phase 1.1

---
+++ src/test/kotlin/com/kip/cms/repository/UserRepositoryTest.kt
@@ -0,0 +1,50 @@
+package com.kip.cms.repository
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+
+@DataJpaTest
+class UserRepositoryTest{
+
+    @Test
+    fun `should save and retrieve user with valid
+data`(){}
+
+　　 @Test
+    fun `sshould validate required username field
+data`(){}
+
+　　 @Test
+    fun `hould validate required email field
+data`(){}
+
+　　 @Test
+    fun `should enforce unique username constraint
+data`(){}
+
+　　 @Test
+    fun `should enforce unique email constraint
+data`(){}
+
+　　 @Test
+    fun `should hash password before saving
+data`(){}
+
+    @Test
+    fun `should save user with different roles
+data`(){}
+
+　　 @Test
+    fun `should find user by username
+data`(){}
+
+　　 @Test
+    fun `hould find user by email
+data`(){}
+　　
+  
+
+
+
+}
